2ff179643b07ba230ce0f6eea4355d8d3bb642e7
raissa-eugenio <eugenio.r@aluno.ifsp.edu.br>
2025-03-18 14:56:42 -0300

    Adicionando um pouco do js

--- js/script.js
+++ js/script.js
@@ -4,3 +4,34 @@ let descricao = document.querySelector('.d-1-4')
 let aviso = document.querySelector('.d-2')
 let lateral = document.querySelector('.d-1-right')
 let numeros = document.querySelector('.d-1-3')
+
+let etapaAtual = 0
+
+function comecarEtapa(){
+    let etapa = etapas[etapaAtual]
+    let numeroHtml = ''
+
+    seuVotoPara.style.display = 'none'
+    cargo.innerHTML = etapa.titulo
+    descricao.innerHTML = ''
+    aviso.style.display = 'none'
+    lateral.innerHTML = ''
+    numeros.innerHTML = numeroHtml
+}
+
+
+function clicou(n) {
+    alert("clicou em " + n )
+}
+
+function branco(){
+    alert("clicou em BRANCO")
+}
+
+function corrige(){
+    alert("clicou em CORRIGE")
+}
+
+comecarEtapa(){
+    
+}
